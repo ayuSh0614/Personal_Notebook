04f7ae548c08c08d37bb6ff3bcd9064f43c3015c
ayuSh0614 <ayusharma051@gmail.com>
2024-03-02 16:15:28 +0530

    commitmentss

---
+++ src/components/Notes.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import noteContext from "../context/notes/noteContext"
+import { useContext } from 'react'
+import NoteItem from './NoteItem';
+
+const Notes = () => {
+    
+  const context = useContext(noteContext);
+  const {notes, setNotes} = context;
+  return (
+    <div className="row my-3">
+      <h2>Your Note</h2>
+      {notes.map((note)=>{
+        return <NoteItem note={note}/>;
+      })}
+    </div>
+  )
+}
+
+export default Notes
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -3,22 +3,75 @@ import NoteContext from "./noteContext";
 import { useState } from "react";
 
 const NoteState = (props) => {
-    const s1 = {
-        "name" : "Harry",
-        "class" : "5b"
-    }
+    const notesInitial = [
+        {
+          "_id": "65d5c202f1809ecde634d7a7",
+          "user": "65d3276bae8aaef1ec67711d",
+          "title": "New note",
+          "description": "Please access the playlist",
+          "tag": "Youtube",
+          "date": "2024-02-21T09:27:30.566Z",
+          "__v": 0
+        },
+        {
+          "_id": "65d5c202f1809ecde634d7a7",
+          "user": "65d3276bae8aaef1ec67711d",
+          "title": "New note",
+          "description": "Please access the playlist",
+          "tag": "Youtube",
+          "date": "2024-02-21T09:27:30.566Z",
+          "__v": 0
+        },
+        {
+          "_id": "65d5c202f1809ecde634d7a7",
+          "user": "65d3276bae8aaef1ec67711d",
+          "title": "New note",
+          "description": "Please access the playlist",
+          "tag": "Youtube",
+          "date": "2024-02-21T09:27:30.566Z",
+          "__v": 0
+        },
+        {
+          "_id": "65d5c202f1809ecde634d7a7",
+          "user": "65d3276bae8aaef1ec67711d",
+          "title": "New note",
+          "description": "Please access the playlist",
+          "tag": "Youtube",
+          "date": "2024-02-21T09:27:30.566Z",
+          "__v": 0
+        },
+        {
+          "_id": "65d5c202f1809ecde634d7a7",
+          "user": "65d3276bae8aaef1ec67711d",
+          "title": "New note",
+          "description": "Please access the playlist",
+          "tag": "Youtube",
+          "date": "2024-02-21T09:27:30.566Z",
+          "__v": 0
+        },
+        {
+          "_id": "65d5c202f1809ecde634d7a7",
+          "user": "65d3276bae8aaef1ec67711d",
+          "title": "New note",
+          "description": "Please access the playlist",
+          "tag": "Youtube",
+          "date": "2024-02-21T09:27:30.566Z",
+          "__v": 0
+        },
+        {
+          "_id": "65d5c202f1809ecde634d7a7",
+          "user": "65d3276bae8aaef1ec67711d",
+          "title": "New note",
+          "description": "Please access the playlist",
+          "tag": "Youtube",
+          "date": "2024-02-21T09:27:30.566Z",
+          "__v": 0
+        }
+      ]
 
-    const [state, setState] = useState(s1);
-    const update = () => {
-        setTimeout(() => {
-            setState({
-                "name" : "larry",
-                "class" : "10b"
-            })
-        }, 1000);
-    }
+      const [notes, setNotes] = useState(notesInitial)
     return (
-        <NoteContext.Provider value={{state, update}}>
+        <NoteContext.Provider value={{notes, setNotes}}>
             {props.children}
         </NoteContext.Provider>
     )
